2401154a7bd9797d9650f8bcf84ab3e49bf9beba
Tucker Morgan <Tucker.bull.morgan@gmail.com>
2021-12-02 17:28:18 -0800

    Day two problem solved

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ where P: AsRef<Path>, {
 
 fn day_1() {
     // File hosts must exist in current path before this produces output
-    let mut first = false;
-    let mut previous = 0;
-    let mut total_increase = 0;
     if let Ok(lines) = read_lines("./day_1_input.txt") {
+        let mut first = false;
+        let mut previous = 0;
+        let mut total_increase = 0;
+    
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
@@ -28,11 +29,43 @@ fn day_1() {
                 previous = value_as_int;
             }
         }
+        println!("{}", total_increase)
     }
-    print!("{}", total_increase)
+
+}
+
+fn day_2() {
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines("./day_1_input.txt") {
+        // Consumes the iterator, returns an (Optional) String
+
+        let mut total_increase = 0;
+
+        let mut all_numbers = vec![];
+        for line in lines {
+            if let Ok(ip) = line {
+                let value_as_int = ip.parse::<i32>().unwrap();
+                all_numbers.push(value_as_int);
+            }
+        }
+
+        let mapped_values : Vec<i32> = all_numbers.windows(3).into_iter().map(|a|{
+            return a[0] + a[1] + a[2];
+        }).collect();
+        let mut previous = mapped_values[0];
+        for value in mapped_values {
+            if value > previous {
+                total_increase += 1;
+            }
+            previous = value;
+        }
+
+        println!("{}", total_increase)
+    }
+
 }
 
 fn main() {
-    println!("Hello, world!");
     day_1();
+    day_2();
 }
